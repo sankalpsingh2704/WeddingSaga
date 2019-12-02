551db02c55f5699772900ebe8fa096c672112b10
sankalpnarayan27 <56334939+sankalpnarayan27@users.noreply.github.com>
2019-12-02 12:00:40 +0530

    sw update

--- public/js/serviceWorker.js
+++ public/js/serviceWorker.js
@@ -1,7 +1,7 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
-  '/',
-  '/public/js/main.js'
+  '/WeddingSaga',
+  '/WeddingSaga/public/js/main.js'
 ];
 
 self.addEventListener('install', function(event) {
